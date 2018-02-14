2fcad49eb5be8847a332eaaaee689d5fbe81cfef
Kostya12rus <mkg97@yandex.ru>
2018-02-14 18:09:11 +0300

    CritHack add new update

--- CritHack.lua
+++ CritHack.lua
@@ -1,21 +1,18 @@
 local CritAttack = {}
-CritAttack.optionEnable = Menu.AddOption({"Kostya12rus", "AutoCrit"}, "Activation", "")
-CritAttack.key = Menu.AddKeyOption({"Kostya12rus","AutoCrit"},"Key for critattak only target",Enum.ButtonCode.BUTTON_CODE_NONE)
-CritAttack.font = Renderer.LoadFont("Tahoma", 50, Enum.FontWeight.EXTRABOLD)
-
+CritAttack.optionEnable = Menu.AddOption({"Utility", "CritHack"}, "Activation", "")
+CritAttack.key = Menu.AddKeyOption({"Utility","CritHack"},"Key for critattak only target",Enum.ButtonCode.BUTTON_CODE_NONE)
 
 function CritAttack.OnUnitAnimation(animation)
 	if not Menu.IsEnabled(CritAttack.optionEnable) then return end
 	if not Menu.IsKeyDown(CritAttack.key) then return end
 	local myHero = Heroes.GetLocal()
 	if not myHero then return end
-	if NPC.GetUnitName(animation.unit) == NPC.GetUnitName(myHero) then
+	if animation.unit == myHero then
 		for _,j in pairs(critattak) do
 			if j == animation.sequenceName then
 				timer = GameRules.GetGameTime() + NPC.GetAttackTime(myHero)/2
 			end
 		end
-		
 		for _,k in pairs(attacanim) do
 			if k == animation.sequenceName then
 				Player.PrepareUnitOrders(Players.GetLocal(), Enum.UnitOrder.DOTA_UNIT_ORDER_STOP, myHero, Vector(0,0,0), nil, Enum.PlayerOrderIssuer.DOTA_ORDER_ISSUER_HERO_ONLY)
@@ -40,7 +37,6 @@ function CritAttack.OnUpdate()
 	local myHero = Heroes.GetLocal()
 	if not myHero then return end
 	if Menu.IsKeyDown(CritAttack.key) then
-		--target = Input.GetNearestHeroToCursor(Entity.GetTeamNum(myHero), Enum.TeamType.TEAM_ENEMY)
 		if target then
 			if Entity.IsAlive(target) then
 				if timer <= GameRules.GetGameTime() then
@@ -57,17 +53,15 @@ function CritAttack.OnUpdate()
 end
 
 function CritAttack.init()
-	attackcrit = false
 	target = nil
 	timer = 0
 	critattak = {
 	 "phantom_assassin_attack_crit_anim"
 	,"attack_crit_anim"
 	,"attack_crit_alt_anim"
-	,"crit"
 	}
 	attacanim = {
-	 "attack01_fast"
+	"attack01_fast"
 	,"attack02_fast"
 	,"attack01_faster"
 	,"attack02_faster"
