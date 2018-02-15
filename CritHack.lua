ee469daf16e6c9fa66ee97aba9db7325ff90dad8
Kostya12rus <mkg97@yandex.ru>
2018-02-15 19:50:20 +0300

    CritHakeUpdateTiming
    New timing for 5 hero

--- CritHack.lua
+++ CritHack.lua
@@ -19,8 +19,6 @@ end
 function CritAttack.OnPrepareUnitOrders(orders)
 	if orders.order == 4 then
 		target = orders.target
-	elseif orders.order == 3 then
-		
 	else
 		target = nil
 	end
@@ -35,12 +33,104 @@ function CritAttack.OnUpdate()
 		if target then
 			if Entity.IsAlive(target) then
 				if timer <= GameRules.GetGameTime() then
-					Player.AttackTarget(Players.GetLocal(),myHero,target)
-					if NPC.GetAttackTime(myHero)/2.5 < 0.3 then
-						timer = GameRules.GetGameTime() + 0.3
-					else
-						timer = GameRules.GetGameTime() + NPC.GetAttackTime(myHero)/2.3
+					if NPC.GetUnitName(Heroes.GetLocal()) == "npc_dota_hero_phantom_assassin" then
+						if NPC.GetAttackTime(myHero)<0.29 then
+							timer = GameRules.GetGameTime() + 0.14
+						elseif NPC.GetAttackTime(myHero) < 0.35 then
+							timer = GameRules.GetGameTime() + 0.15
+						elseif NPC.GetAttackTime(myHero) < 0.54 then
+							timer = GameRules.GetGameTime() + 0.17
+						elseif NPC.GetAttackTime(myHero) < 0.71 then
+							timer = GameRules.GetGameTime() + 0.20
+						elseif NPC.GetAttackTime(myHero) < 0.86 then
+							timer = GameRules.GetGameTime() + 0.24
+						elseif NPC.GetAttackTime(myHero) < 1.08 then
+							timer = GameRules.GetGameTime() + 0.27
+						elseif NPC.GetAttackTime(myHero) < 1.24 then
+							timer = GameRules.GetGameTime() + 0.3
+						end
+					elseif NPC.GetUnitName(Heroes.GetLocal()) == "npc_dota_hero_skeleton_king" then 
+						if NPC.GetAttackTime(myHero) < 0.29 then
+							timer = GameRules.GetGameTime() + 0.175
+						elseif NPC.GetAttackTime(myHero) < 0.34 then
+							timer = GameRules.GetGameTime() + 0.2
+						elseif NPC.GetAttackTime(myHero) < 0.44 then
+							timer = GameRules.GetGameTime() + 0.24
+						elseif NPC.GetAttackTime(myHero) < 0.54 then
+							timer = GameRules.GetGameTime() + 0.27
+						elseif NPC.GetAttackTime(myHero) < 0.67 then
+							timer = GameRules.GetGameTime() + 0.3
+						elseif NPC.GetAttackTime(myHero) < 0.79 then
+							timer = GameRules.GetGameTime() + 0.32
+						elseif NPC.GetAttackTime(myHero) < 0.87 then
+							timer = GameRules.GetGameTime() + 0.37
+						elseif NPC.GetAttackTime(myHero) < 0.96 then
+							timer = GameRules.GetGameTime() + 0.4
+						elseif NPC.GetAttackTime(myHero) < 1.10 then
+							timer = GameRules.GetGameTime() + 0.44
+						elseif NPC.GetAttackTime(myHero) < 1.20 then
+							timer = GameRules.GetGameTime() + 0.47
+						elseif NPC.GetAttackTime(myHero) < 1.45 then
+							timer = GameRules.GetGameTime() + 0.57
+						end
+					elseif NPC.GetUnitName(Heroes.GetLocal()) == "npc_dota_hero_juggernaut" then
+						if NPC.GetAttackTime(myHero) < 0.27 then
+							timer = GameRules.GetGameTime() + 0.14
+						elseif NPC.GetAttackTime(myHero) < 0.39 then
+							timer = GameRules.GetGameTime() + 0.17
+						elseif NPC.GetAttackTime(myHero) < 0.48 then
+							timer = GameRules.GetGameTime() + 0.20
+						elseif NPC.GetAttackTime(myHero) < 0.69 then
+							timer = GameRules.GetGameTime() + 0.24
+						elseif NPC.GetAttackTime(myHero) < 0.85 then
+							timer = GameRules.GetGameTime() + 0.27
+						elseif NPC.GetAttackTime(myHero) < 0.97 then
+							timer = GameRules.GetGameTime() + 0.30
+						elseif NPC.GetAttackTime(myHero) < 1.12 then
+							timer = GameRules.GetGameTime() + 0.34
+						end
+					elseif NPC.GetUnitName(Heroes.GetLocal()) == "npc_dota_hero_chaos_knight" then
+						if NPC.GetAttackTime(myHero) < 0.33 then
+							timer = GameRules.GetGameTime() + 0.17
+						elseif NPC.GetAttackTime(myHero) < 0.38 then
+							timer = GameRules.GetGameTime() + 0.2
+						elseif NPC.GetAttackTime(myHero) < 0.42 then
+							timer = GameRules.GetGameTime() + 0.21
+						elseif NPC.GetAttackTime(myHero) < 0.48 then
+							timer = GameRules.GetGameTime() + 0.24
+						elseif NPC.GetAttackTime(myHero) < 0.67 then
+							timer = GameRules.GetGameTime() + 0.27
+						elseif NPC.GetAttackTime(myHero) < 0.79 then
+							timer = GameRules.GetGameTime() + 0.31
+						elseif NPC.GetAttackTime(myHero) < 0.87 then
+							timer = GameRules.GetGameTime() + 0.35
+						elseif NPC.GetAttackTime(myHero) < 0.96 then
+							timer = GameRules.GetGameTime() + 0.37
+						elseif NPC.GetAttackTime(myHero) < 1.10 then
+							timer = GameRules.GetGameTime() + 0.41
+						elseif NPC.GetAttackTime(myHero) < 1.28 then
+							timer = GameRules.GetGameTime() + 0.44
+						elseif NPC.GetAttackTime(myHero) < 1.5 then
+							timer = GameRules.GetGameTime() + 0.54
+						end
+					elseif NPC.GetUnitName(Heroes.GetLocal()) == "npc_dota_hero_brewmaster" then  
+						if NPC.GetAttackTime(myHero) < 0.33 then
+							timer = GameRules.GetGameTime() + 0.14
+						elseif NPC.GetAttackTime(myHero) < 0.47 then
+							timer = GameRules.GetGameTime() + 0.17
+						elseif NPC.GetAttackTime(myHero) < 0.65 then
+							timer = GameRules.GetGameTime() + 0.20
+						elseif NPC.GetAttackTime(myHero) < 0.78 then
+							timer = GameRules.GetGameTime() + 0.25
+						elseif NPC.GetAttackTime(myHero) < 0.94 then
+							timer = GameRules.GetGameTime() + 0.28
+						elseif NPC.GetAttackTime(myHero) < 1.21 then
+							timer = GameRules.GetGameTime() + 0.30
+						elseif NPC.GetAttackTime(myHero) < 1.4 then
+							timer = GameRules.GetGameTime() + 0.37
+						end
 					end
+					Player.AttackTarget(Players.GetLocal(),myHero,target)
 				end
 			end
 		end
